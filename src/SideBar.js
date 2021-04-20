cf30d76aedf5a60c54a10adc9f17557eb4420969
vermad843 <vishnuverma7524@gmail.com>
2021-04-20 14:04:23 +0530

    sidebarRow.css

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import './Sidebar.css';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import SidebarRow from './SidebarRow';
 import HomeIcon from '@material-ui/icons/Home';
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import Button from '@material-ui/core/Button';
+import './Sidebar.css';
+
 
 function SideBar() {
     return (
         <div className = "sidebar">
-            <TwitterIcon className = "sidebar__icon"/>
-            <SidebarRow Icon= {HomeIcon} title = "Home"/>
-            <SidebarRow Icon = {NotificationsNoneIcon} title = "NotificationsNone"/>
-            <SidebarRow Icon = {MailOutlineIcon} title = "MailOutline"/>      
+            <SidebarRow Icon=  {TwitterIcon}/>
+            <SidebarRow Icon=  {HomeIcon} title = "Home"/>
+            <SidebarRow Icon = {NotificationsNoneIcon} title = "Notifications"/>
+            <SidebarRow Icon = {MailOutlineIcon} title = "Messages"/> 
+            <SidebarRow Icon = {BookmarkBorderIcon} title = "Bookmarks"/>
+            <SidebarRow Icon = {ListAltIcon} title = "Lists"/> 
+            <SidebarRow Icon = {PersonOutlineIcon} title = "Profile"/>
+            <SidebarRow Icon = {MoreHorizIcon} title = "More"/> 
+            <Button 
+               variant="contained"
+               color = "primary"
+               size = "large"
+               >
+               Tweet    
+            </Button> 
         </div>
     )
 }
--- src/SidebarRow.js
+++ src/SidebarRow.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './Sidebar.css';
+import './SidebarRow.css';
 
 function SidebarRow  ({Icon, title}) {
     return (
         <div className = "sidebarRow">
-            <Icon/>
+            <Icon className = "sidebarRow__icon"/>
             <h2>{title}</h2>
         </div>
     );
