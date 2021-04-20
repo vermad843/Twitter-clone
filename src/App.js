8761a39019bfa7fb79a46d9631a17db26e1038f6
vermad843 <vishnuverma7524@gmail.com>
2021-04-20 12:12:13 +0530

    sidebar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Sidebar from './SideBar';
 
 function App() {
   return (
     <div className="app">
        <Header/>
+       <Sidebar/>
     </div>
   );
 }
---
+++ src/SideBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Sidebar.css';
+
+function SideBar() {
+    return (
+        <div>
+            <h1>I am a Sidebar Component</h1>
+        </div>
+    )
+}
+
+export default SideBar
