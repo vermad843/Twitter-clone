b5c6e23bc337e352b5dc041543cd535db9f72fd5
vermad843 <vishnuverma7524@gmail.com>
2021-04-21 10:36:39 +0530

    design

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Sidebar from './SideBar';
 function App() {
   return (
     <div className="app">
+       <Sidebar/>
        <Header/>
        <Sidebar/>
     </div>
--- src/Header.js
+++ src/Header.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import './Header.css';
-import TwitterIcon from '@material-ui/icons/Twitter';
-import SearchIcon from '@material-ui/icons/Search';
+// import SearchIcon from '@material-ui/icons/Search';
 
 function Header() {
     return (
         <div className = "header">
-            <div className = "header__left">
+            {/* <div className = "header__left">
                <TwitterIcon/>    
-            </div>
+            </div> */}
             <div className = "header__mid">
                 <h1>Home</h1>
             </div>
-            <div className = "header__right">
+            {/* <div className = "header__right">
                <SearchIcon
                   className = "header__rightButton"
                 />            
                <input                 
                  placeholder = "Search Twitter"
                 />
-            </div>
+            </div> */}
         </div>
     )
 }
