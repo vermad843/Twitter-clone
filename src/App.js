4936143f4d3cac3902171d80feff2f994df263b0
vermad843 <vishnuverma7524@gmail.com>
2021-04-21 10:42:32 +0530

    sidebarRight component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './SideBar';
+import SidebarRight from './SidebarRight';
 
 function App() {
   return (
     <div className="app">
        <Sidebar/>
        <Header/>
-       <Sidebar/>
+       <SidebarRight/>
     </div>
   );
 }
---
+++ src/SidebarRight.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SidebarRight.css';
+
+function SidebarRight() {
+    return (
+        <div className = "sidebarRight">
+            <h1>I am a SidebarRight Component</h1>
+        </div>
+    )
+}
+
+export default SidebarRight
